45ac2334eca062a5c7d12478d7aa0ce2ca21da29
hadjuse <104326150+hadjuse@users.noreply.github.com>
2022-04-30 19:10:00 +0200

    Add files via upload

--- plateau.c
+++ plateau.c
@@ -1,19 +1,34 @@
+//  this is file which contains every function that will be use on the file main.c
 #include <stdio.h>
 #include "plateau.h"
-#define LINE 10
-#define COLUMN 10
-typedef struct Block
-{
-    int orientation;
-};
 
-typedef struct Game
+// This function will fill a bi-dimensionnal tab which will be use to containing blocks
+//in the game
+void Generate(char tab[LINE][COLUMN])
 {
-    char plateau[COLUMN][LINE];
-    Block block;
-};
-
-Game Generate()
+    for (int l = 0; l<LINE; l++)
+    {
+        for (int c = 0; c<COLUMN; c++)
+        {
+            tab[LINE][COLUMN] = ' ';
+        }
+    }
+}
+//This function show the grid during the whole game
+void show_grid(char tab[LINE][COLUMN])
 {
-    
+    printf("This is your grid:\n");
+    // I made the 22th line in 1 line beause there is only one instruction
+    for (int i=0; i<10; i++){printf(" %c", (char)65+i);}
+    printf("\n");
+    for (int l = 0; l<LINE; l++)
+    {
+        for (int c = 0; c<COLUMN; c++)
+        {
+            printf("|");
+            printf("%c", tab[LINE][COLUMN]);
+        }
+        printf("|");
+        printf("\n");
+    }
 }
